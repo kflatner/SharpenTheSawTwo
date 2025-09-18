3a9b24979effea76ee6e506c85d42ed6c851e9d9
kflatner <kflatner3@gmail.com>
2025-09-18 14:34:11 +0200

    Changed/Two

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 
 module.exports = defineConfig({
-  publicPath: '/SharpenthesawFrontend/',
+  publicPath: '/SharpenTheSawTwo/',
   transpileDependencies: true,
   devServer: {
     proxy: {
